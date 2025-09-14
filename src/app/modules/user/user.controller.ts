d046c1e589e29bfd113e6579f3bc5028f13ef055
Alamin <alamin.kzs06@gmail.com>
2025-09-15 00:26:26 +0600

    Auth System Added

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,107 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status-codes";
+import { UserServices } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import { verifyToken } from "../../utils/jwt";
+import { envVars } from "../config/env";
+import { JwtPayload } from "jsonwebtoken";
+
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = await UserServices.createUser(req.body);
+
+    // res.status(httpStatus.CREATED).json({
+    //   message: "User creates successfully!",
+    //   user,
+    // });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User creates successfully!",
+      data: user,
+    });
+  }
+);
+
+const updateUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.params.id;
+    // const token = req.headers.authorization
+    // const verifiedToken = verifyToken(token as string, envVars.JWT_ACCESS_SECRET) as JwtPayload
+
+    const verifiedToken = req.user;
+
+    const payload = req.body;
+    const user = await UserServices.updateUser(userId, payload, verifiedToken);
+
+    // res.status(httpStatus.CREATED).json({
+    //     message: "User Created Successfully",
+    //     user
+    // })
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User Updated Successfully",
+      data: user,
+    });
+  }
+);
+
+const getAllUsers = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await UserServices.getAllUsers();
+
+    // res.status(httpStatus.OK).json({
+    //   success: true,
+    //   message: "All users retrieved successfully!",
+    //   data: users,
+    // });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "All users retrieved successfully!",
+      data: result.data,
+      meta: result.meta,
+    });
+  }
+);
+
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     // throw new Error("Fake err");
+//     // throw new AppError(httpStatus.BAD_REQUEST, "Fake err");
+//     const user = await UserServices.createUser(req.body);
+
+//     res.status(httpStatus.CREATED).json({
+//       message: "User creates successfully",
+//       user,
+//     });
+
+//   } catch (err: any) {
+//     console.log(err);
+//     next(err);
+//   }
+// };
+
+// const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     const users = await UserServices.getAllUsers();
+
+//     return users;
+//   } catch (err: any) {
+//     console.log(err);
+//     next();
+//   }
+// };
+
+export const UserControllers = {
+  createUser,
+  updateUser,
+  getAllUsers,
+};
